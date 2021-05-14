cba2c3faffe91c6e9c8dce8e5709b5dd601e9392
RoiB <richard.ding.gt@gmail.com>
2021-05-14 01:13:24 +0000

    finish hw 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,75 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr
+    return arr.inject(0){|total,x| total+=x}
+  else
+    return 0
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    return arr.sort_by{|x| -x}.first(2).inject(:+)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  hash = Hash.new
+  arr.each do |x|
+    if hash.include?(x)
+      return true
+    else
+      hash[n-x] = x
+    end
+  end
+    false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.length==0
+    return false
+  else
+    # !"aeiou".include?(s.split("").first.downcase)
+    return s[0].downcase[/[b-df-hj-np-tv-z]/] != nil
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0
+    return false
+  elsif s[/[^01]/] == nil
+    return s.to_i % 4 == 0
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize(isbn, price)
+    if isbn == "" or price <= 0
+      raise ArgumentError 
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    "$%.2f" % price
+  end
 end
